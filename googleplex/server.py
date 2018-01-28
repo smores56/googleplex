fdc051f6274745a95ad00bbb3e96e63d1272f85c
Sam Mohr <sammohr97@gmail.com>
2018-01-28 12:41:50 -0500

    changed imports, added shebang, added routes

--- googleplex/server.py
+++ googleplex/server.py
@@ -1,48 +1,103 @@
+#!/usr/bin/env python3
+
 import os
-from datetime import date
-from googleplex import app
-from .data import load_bestsellers
-from .models import Bestseller, BestsellerList, Review
-from flask import send_from_directory, render_template, request
+from .models import *
+from .util import *
+
+from peewee import DoesNotExist
+from sanic import Sanic
+from sanic.response import *
+import sanic_cookiesession
+
+CONFIG = load_config()
+app = Sanic(__name__)
+
+app.config['SESSION_COOKIE_SECRET_KEY'] = CONFIG['SESSION_COOKIE_SECRET_KEY']
+sanic_cookiesession.setup(app)
+
 
 @app.route('/')
 @app.route('/index')
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+async def index(request):
+    user = User.load_if_logged_in(request)
+    return render_template('index.html', user=user)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+async def login(request):
+    if request.method == 'GET':
+        session = request['session']
+        flash = session.popitem()[1] if 'flash' in session else None
+        user = User.load_if_logged_in(request)
+
+        return redirect('/') if user else render_template('login.html', flash=flash)
+
+    else:
+        email = request.form.get('email')
+        pass_hash = request.form.get('pass_hash')
+        re_hash = scrypt(pass_hash, (email + 'xyz')[:8])
+        try:
+            user = User.get(email=email, pass_hash=re_hash)
+            session = Session(user)
+            request['session']['login_cookie'] = str(session)
+            return redirect('/')
+
+        except DoesNotExist:
+            request['session']['flash'] = 'The given email/password combination does \
+                                           not match any user in our records.'
+            return redirect('/login')
+
 
 @app.route('/favicon.ico')
-def favicon():
+async def favicon(request):
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
 
+
+@app.route('/scripts/<script>')
+async def scripts(request, script):
+    return send_from_directory(os.path.join(app.root_path, 'static/scripts/'), script)
+
+
+@app.route('/styles/<sheet>')
+async def styles(request, sheet):
+    return send_from_directory(os.path.join(app.root_path, 'static/styles/'), sheet)
+
+
 @app.route('/profile')
-@app.route('/profile.html')
-def profile():
+@authorized()
+async def profile(request, user):
     return render_template('profile.html')
 
+
 @app.route('/submit')
-@app.route('/submit.html')
-def submit():
+@authorized()
+async def submit(request, user):
     return render_template('submit.html')
 
+
 @app.route('/manual_submit')
-@app.route('/manual_submit.html')
-def manual_submit():
+@authorized()
+async def manual_submit(request, user):
     return render_template('manual_submit.html')
 
+
 @app.route('/preview')
-@app.route('/preview.html')
-def preview():
+@authorized()
+async def preview(request, user):
     return render_template('preview.html')
 
+
 @app.route('/results')
-@app.route('/results.html')
-def results():
+@authorized()
+async def results(request, user):
     search = request.args.get('search') or ''
-    return render_template('results.html', lists=search_db(search), search=search)
+    return render_template('results.html', lists=BestsellerList.search(search), search=search)
 
-def search_db(name):
-    return [l for l in DATABASE_DATA if name.lower() in l.name.lower()]
 
-DATABASE_DATA = load_bestsellers()
+def run():
+    app.run(host='127.0.0.1', port='5678')
+
+
+if __name__ == '__main__':
+    run()
