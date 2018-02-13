d9343e6cb172711616cda8f12eb9e3d125176b24
Sam Mohr <sammohr97@gmail.com>
2018-02-12 23:50:42 -0500

    renamed to allow more general data, updated user data to work with login

--- add_test_data.py
+++ add_test_data.py
@@ -2,12 +2,12 @@ from googleplex import models
 
 user_data = {
     'admin': True,
-    'email': 'abc@gmail.com',
+    'email': 'gpurdell@gatech.edu',
     'first_name': 'George',
     'institution': 'Georgia Tech',
     'is_banned': False,
     'last_name': 'Burdell',
-    'pass_hash': 'ABCDEFG',
+    'pass_hash': '9bb12650f91df864a09588e397327c26c20cc348f3d32847684971830828637e',
     'position': 'Living Legend',
     'premium': True
 }
