1b163cb3445b04bc59b7c54e8460b4f1f8b99f55
Marc <mlacayo6@gatech.edu>
2018-02-13 22:57:47 -0500

    fixed add_test_books.py

--- add_test_books.py
+++ add_test_books.py
@@ -1,13 +1,15 @@
 from googleplex import models
 
 book_data = {
-    "title" : "Test Book"
+    "title" : "Test Book",
+    "bestseller_list" : 2
 }
 
 models.Bestseller.create(**book_data)
 
 book_data = {
-    "title" : "Please Ignore"
+    "title" : "Please Ignore",
+    "bestseller_list" : 3
 }
 
 models.Bestseller.create(**book_data)
