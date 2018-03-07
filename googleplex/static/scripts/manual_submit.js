ae3374c39efc027c9f93509b235c91f6fc4206dc
Sam Mohr <sammohr97@gmail.com>
2018-03-07 00:56:31 -0500

    fixed json layout on submission, small formatting

--- googleplex/static/scripts/manual_submit.js
+++ googleplex/static/scripts/manual_submit.js
@@ -1,63 +1,61 @@
 $(document).ready(function() {
-    // put your Javascript here
-    // $("div").slice(0, 2).show(); // select the first ten
+  // put your Javascript here
+  // $("div").slice(0, 2).show(); // select the first ten
 
-    var title = 1;
-    var author = 1;
-    var year = 1;
-    $("#load").click(function () {
-      $('#list tr:last').after('<br/><br/><tr>' +
-          '<td>' +
-            '<table style="width:80%">' +
-              '<tr>' +
-                '<td>' +
-                  '<span class="col-lg-3" style="text-align:left">Title: </span>' +
-                  '<input type="text" id="bookTitle' + (title+=1) + '">' +
-                '</td>' +
-              '</tr>' +
-              '<tr>' +
-                '<td>' +
-                  '<span class="col-lg-4" style="text-align:left">Author: </span>' +
-                  '<input type="text" id="author' + (author+=1) + '">' +
-                '</td>' +
-              '</tr>' +
-              '<tr>' +
-                '<td>' +
-                  '<span class="col-lg-4" style="text-align:left">Year Published: </span>' +
-                  '<input type="text" id="yearPublished' + (year+=1) + '">' +
-                '</td>' +
-              '</tr>' +
-            '</table>' +
-            '</td>' +
-        '</tr>');
+  var num_bestsellers = 1;
+  $("#load").click(function() {
+    num_bestsellers++;
+    $('#list tr:last').after(`<br/><br/><tr>
+          <td>
+            <table style="width:80%">
+              <tr>
+                <td>
+                  <span class="col-lg-3" style="text-align:left">Title: </span>
+                  <input type="text" id="title` + num_bestsellers + `">
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span class="col-lg-4" style="text-align:left">Author: </span>
+                  <input type="text" id="author` + num_bestsellers + `">
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span class="col-lg-4" style="text-align:left">Date Published: </span>
+                  <input type="date" id="authored_date` + num_bestsellers + `">
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>`);
+  });
+
+  $('#manual-submit').submit(function(ev) {
+    ev.preventDefault();
+
+    var data = {
+      bestsellers: [],
+      title: $("#title").val(),
+      num_bestsellers: num_bestsellers
+    };
+    for (i = 1; i < num_bestsellers + 1; i++) {
+      data["bestsellers"].push({
+        title: $("#title" + i).val(),
+        author: $("#author" + i).val(),
+        authored_date: $("#authored_date" + i).val()
       });
-
-      $('#manual-submit').submit(function(ev) {
-        ev.preventDefault();
-
-        var length = $("#books tr").length;
-        var numBooks = length / 3;
-        var data = {};
-        for (i = 1; i < numBooks + 1; i++) {
-          data["book".concat(i)] = {
-            bookTitle: $("#bookTitle" + i).val(),
-            author: $("#author" + i).val(),
-            yearPublished: $("#yearPublished" + i).val()
-          };
-        }
-        data['list_title'] = $("#listTitle").val()
-        data['book_count'] = numBooks;
-        $.ajax({
-          url: '/manualpreview',
-          data: JSON.stringify(data),
-          type: "POST",
-          success: function(response) {
-            window.location.href = '/preview';
-          },
-          error: function(xhr) {
-            console.trace()
-          }
-        });
-      });
-
+    }
+    $.ajax({
+      url: '/manual_submit?type=manual',
+      data: JSON.stringify(data),
+      type: "POST",
+      success: function(response) {
+        window.location.href = '/preview?type=manual&list_id=' + response.list_id;
+      },
+      error: function(xhr) {
+        console.trace()
+      }
+    });
+  });
 });
