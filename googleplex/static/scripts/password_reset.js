4c21f2b8acf13281d550e3c54b6bbc4bf2be3c3a
Sam Mohr <sammohr97@gmail.com>
2018-03-29 18:49:26 -0400

    finished page for password resetting (requesting and submitting)

--- googleplex/static/scripts/password_reset.js
+++ googleplex/static/scripts/password_reset.js
@@ -1,19 +1,14 @@
-function submit_reset() {
-  var valid = validate_reset();
-  if (valid){
-    document.getElementById('password-reset-form').submit(); //form submission
+function request_reset() {
+  var valid = validate_email();
+  if (valid) {
+    document.getElementById('request-reset-form').submit(); //form submission
   }
 
   console.log('submit_registration returned ' + valid);
 
   return valid;
 }
-function validate_reset() {
-  var err_count = 0;
-  if (!validate_email()) err_count++;
 
-  return err_count == 0;
-}
 function validate_email() {
   var email = document.getElementById('email');
   var email_error = document.getElementById('email-error');
@@ -25,4 +20,30 @@ function validate_email() {
     email_error.style.display = 'none';
     return true;
   }
+}
+
+function validate(input) {
+  if (input.value != document.getElementById('new_pass_1').value) {
+    input.setCustomValidity('Password Must be Matching.');
+  } else {
+    // input is valid -- reset the error message
+    input.setCustomValidity('');
+  }
+}
+
+function submit_reset() {
+  var new_pass = document.getElementById('new_pass_1').value;
+  var email = document.getElementById('user_email').value;
+
+  scrypt(new_pass, (email + 'xyz').substring(0, 8), {
+    N: 4,
+    r: 4,
+    p: 2,
+    dkLen: 16,
+    encoding: 'hex'
+  }, function(new_pass_hash) {
+    document.getElementById('new_pass_hash').value = new_pass_hash;
+
+    document.getElementById('password-reset-form').submit(); //form submission
+  });
 }
