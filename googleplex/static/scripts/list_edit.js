9c09dbcd4c7f47166883fb2f857a4c7fe24c9edf
Marc <mlacayo6@gatech.edu>
2018-03-30 02:27:52 -0400

    added more features to list editing, start of form validation

--- googleplex/static/scripts/list_edit.js
+++ googleplex/static/scripts/list_edit.js
@@ -1,16 +1,43 @@
+var bestsellersOffset = 2; //number of items before bestseller title input boxes
+var prevSelected = null;
+var greenBackground = "#99DD99";
+var whiteBackground = "#FFFFFF";
+
+
 $(document).ready(function() {
   // put your Javascript here
-  // $("div").slice(0, 2).show(); // select the first ten
+  labels = document.getElementsByClassName("form_label");
 
-  var num_bestsellers = (document.getElementById("list_form").length - 1)/2; //total inputs - single title input / 2 = total text box inputs
+  for (var i = 0; i < labels.length; i++) {
+      label = labels[i];
+      if (label.id != "add_label") {
+          label.addEventListener('click', function(event) {
+              if (this == event.target) {
+                  event.stopPropagation();
+                  return false;
+              } else {
+                  switchSelect(this);
+              }
+          }, false);
+      }
+  }
 
   $("#load").click(function() {
-    num_bestsellers++;
+    var num_bestsellers = document.getElementById("list_form").children.length - bestsellersOffset + 1;
     form = document.getElementById("list_form");
+
     //add styling label
     var label = document.createElement("label");
     label.classList.add("form_label");
-    label.innerHTML = 'Book ' + num_bestsellers + ': ';
+    label.innerHTML = "                Book " + num_bestsellers + ":\n ";
+    label.addEventListener('click', function(event) {
+        if (this == event.target) {
+            event.stopPropagation();
+            return false;
+        } else {
+            switchSelect(this);
+        }
+    }, false);
     //add actual input box
     var input = document.createElement("input");
     input.type = "text";
@@ -18,27 +45,69 @@ $(document).ready(function() {
     label.appendChild(input);
     //add delete button
     var button = document.createElement("button")
-    button.name = "button" + num_bestsellers;
+    button.id = "button" + num_bestsellers;
     button.innerHTML = "Delete"
     button.type = "button";
-    button.onClick = deleteClick();
+    button.onclick = function() { deleteClick(button.id) };
     label.appendChild(button);
     //textbox and button to form
     form.appendChild(label);
   });
 
   $("#submit").click(function() {
-      document.getElementById("list_form").submit();
+      var num_bestsellers = document.getElementById("list_form").children.length - bestsellersOffset;
+      var listTitle = document.forms["list_form"]["title"];
+      if (listTitle == "") {
+          alert("List requires a title.");
+          return false;
+      }
+
+      //Ensure there is at least one book on the list
+      if (num_bestsellers < 1) {
+          alert("List must have at least one book!");
+          return false;
+      }
+
+      for (var i = 1; i <= num_bestsellers; i++) {
+          var bookName = "book" + i;
+          var bestseller = document.forms["list_form"][bookName].value;
+          if (bestseller == "") {
+              var logStr = "Book " + i + " is empty, please delete it before submitting the list.";
+              alert(logStr);
+          }
+      }
   })
-  // $('#remove').on('click', function() {
-  //   for (i = 0; i < 3; i++) {
-  //     $('#books tr:last').remove();
-  //   }
-  //   num_bestsellers--;
-  // });
+
 });
 
-function deleteClick() {
-  console.log('xdd');
-  //   console.log(this.name);
+function switchSelect(object) {
+    console.log(object);
+    if (!prevSelected) {
+        console.log("Switch select 1");
+        object.style.backgroundColor = greenBackground;
+        prevSelected = object;
+    } else {
+        console.log("Switch select 2");
+        //swap object and prevSelected here
+        objectText = object.children[0].value;
+        object.children[0].value = prevSelected.children[0].value;
+        prevSelected.children[0].value = objectText;
+        prevSelected.style.backgroundColor = whiteBackground;
+        prevSelected = null;
+    }
+}
+
+function deleteClick(clicked_id) {
+    var bookToDelete = document.getElementById(clicked_id).parentElement;
+    bookToDelete.remove();
+    var loopStart = parseInt(clicked_id[6]) + bestsellersOffset - 1;
+    elements = document.getElementById("list_form").children;
+
+    for(var i = loopStart; i < elements.length; i++) {
+        children = elements[i].childNodes;
+        var bookNum = i - 1;
+        children[0].data = "                Book " + bookNum + ":\n "
+        children[1].name = "book" + bookNum;
+        children[2].id = "button" + bookNum;
+    }
 };
